de83ce6889395866022f39113668f127e5a4a311
Pyrokryos <dulina.perera@outlook.com>
2024-07-09 16:26:39 +0530

    Enhanced the post headers using metadata from the markdown documents.

---
+++ with-svelte/src/lib/components/PostHeader.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { formatPublishedAt } from "$lib/utils/dates";
+	import type { MarkdownPost } from "../../types";
+
+	export let metadata: MarkdownPost["metadata"];
+
+
+</script>
+
+<header class="p-4">
+	<div class="mb-6 sm:w-3/5 w-full">
+		<div class="aspect-h-9 aspect-w-16 overflow-hidden rounded-lg">
+			{#if metadata.youtubeId}
+				<iframe
+					title={metadata.title}
+					src={`https://www.youtube.com/embed/${metadata.youtubeId}?origin=http://example.com`}
+					allow="fullscreen"
+				/>
+			{:else}
+				<img src={metadata.imgUrl} alt={metadata.title} class="object-cover" />
+			{/if}
+		</div>
+	</div>
+	<div>
+		<h1 class="font-bold mb-4 text-4xl">{metadata.title}</h1>
+		<div
+			class="border-t dark:border-gray-700 inline-block py-2"
+		>
+			<span>Published: </span>
+			<time class="font-light" datetime={metadata.publishedAt}>
+				{formatPublishedAt(metadata.publishedAt)}
+			</time>
+		</div>
+	</div>
+</header>
--- with-svelte/src/routes/blog/posts/[slug]/+page.svelte
+++ with-svelte/src/routes/blog/posts/[slug]/+page.svelte
@@ -2,6 +2,7 @@
 	import './prism-night-owl.css';
 	import type { PageData } from './$types';
 	import CopyCodeInjector from '$lib/components/CopyCodeInjector.svelte';
+	import PostHeader from '$lib/components/PostHeader.svelte';
 
 	export let data: PageData;
 
@@ -9,6 +10,8 @@
 </script>
 
 <article>
+	<PostHeader {metadata} />
+
 	<div class="dark:prose-invert max-w-none prose px-4 py-4">
 		<CopyCodeInjector>
 			<Post />
